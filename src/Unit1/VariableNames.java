e742a4aff737552653b8cb1e4335fb4a4c1f46df
miltondw <estradamilton2001@gmail.com>
2025-08-30 15:19:15 -0500

    Add SDLC diagram and VariableNames class implementation

---
+++ src/Unit1/VariableNames.java
@@ -0,0 +1,30 @@
+package Unit1;
+/*
+ * Ejercicio:
+ *  Declara y asigna valores a variables que representen la edad, altura, inicial del nombre, y si una persona es mayor de edad o no.
+ */
+
+public class VariableNames {
+    public static void main(String[] args) {
+        String name = "Milton Estrada";
+        char gender = 'M';
+        int age = 23;
+        double height = 1.77;
+        float weight = 68.5f;
+        boolean isAdult = age >= 18;
+        byte siblings = 1;
+        final short YEAR_OF_BIRTH = 2001;
+        long phoneNumber = 573001234567L;
+
+        System.out.println("Nombre: " + name);
+        System.out.println("Género: " + gender);
+        System.out.println("Edad: " + age);
+        System.out.println("Altura: " + height + " m");
+        System.out.println("¿Es mayor de edad?: " + (isAdult ? "si" : "no"));
+        System.out.println("Cantidad de hermanos: " + siblings);
+        System.out.println("Año de nacimiento : " + YEAR_OF_BIRTH);
+        System.out.println("Teléfono:  " + phoneNumber);
+        System.out.println("Peso: " + weight + " kg");
+
+    }
+}
